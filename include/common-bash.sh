fb07036b7779345f7c487dd111dc20b5fea2c91f
Bernard Tatin <bernard.tatin@outlook.fr>
2017-10-15 22:58:56 +0200

    Testing dohrlp function, phase onr...

--- include/common-bash.sh
+++ include/common-bash.sh
@@ -1,3 +1,36 @@
 #!/usr/bin/env bash
 
 script_name=$(basename $0)
+
+#!/usr/bin/env bash
+
+function onerror() {
+    local exit_code=$1
+    shift
+    local error_msg="$@"
+
+    echo "ERROR: $error_msg" 1>&2
+    exit $exit_code
+}
+
+function safe_source() {
+  local file=
+  while [ $# -gt 0 ]
+  do
+  file=$1
+    if [ -f ${file} ]
+    then
+      source ${file}
+    else
+      onerror 1 "Cannot source ${file}"
+    fi
+    shift
+  done
+}
+
+function get_tmp_file() {
+  local root_name='another-tmp-file'
+  [ $# -gt 0 ] && \
+    root_name=$1
+  echo $(mktemp /tmp/${root_name}.XXXXXX)
+}
---
+++ tests/tst-biggest-dir.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+bindir=$(cd $(dirname $0); pwd)
+cd ${bindir}
+source ../include/common-bash.sh
+# safe_source ../biggest-dir
+
+tst_help=$(get_tmp_file).sh
+
+cat > ${tst_help} << TST_HELP
+#!/bin/bash
+cd ${bindir}
+cd ..
+bindir=\$(pwd)
+# echo "pwd: \$(pwd), bindir: \${bindir}"
+source include/common-bash.sh
+safe_source biggest-dir
+
+exit_code=0
+[ $# -gt 0 ] && \
+  exit_code=\$1
+
+dohelp \${exit_code} &> /dev/null
+TST_HELP
+
+chmod +x ${tst_help}
+for ec in -1 0 1 2 3
+do
+  echo '-------------------------------------------------------'
+  ${tst_help} ${ec}
+  rvalue=$?
+  if [ "${ec}"="${rvalue}" ]
+  then
+    echo "test ${ec} OK"
+  else
+    echo "test ${ec} BAAAD"
+  fi
+done
+
+rm -fv ${tst_help}
+exit 0
