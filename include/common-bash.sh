24baccfb85edc22c91c7a2c79db33696a8e301b5
Bernard Tatin <bernard.tatin@outlook.fr>
2019-04-29 22:21:24 +0200

    better code indent

--- include/common-bash.sh
+++ include/common-bash.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 export script_name=$(basename $0)
+export PRINTF=$(which printf)
 
+# onerror exit_code message ...
 function onerror() {
     local exit_code=$1
     shift
@@ -11,46 +13,46 @@ function onerror() {
     exit $exit_code
 }
 
+# safe_source file file ...
 function safe_source() {
-  local file=
-  while [ $# -gt 0 ]
-  do
-  file=$1
-    if [ -f ${file} ]
-    then
-      source ${file}
-    else
-      onerror 1 "Cannot source ${file}"
-    fi
-    shift
-  done
+    local file=
+    while [ $# -gt 0 ]
+    do
+        file=$1
+        if [ -f ${file} ]
+        then
+            source ${file}
+        else
+            onerror 1 "Cannot source ${file}"
+        fi
+        shift
+    done
 }
 
 function get_tmp_file() {
-  local root_name='another-tmp-file'
-  [ $# -gt 0 ] && \
-    root_name=$1
-  $(mktemp /tmp/${root_name}.XXXXXX)
+    local root_name='another-tmp-file'
+    [ $# -gt 0 ] && \
+        root_name=$1
+    $(mktemp /tmp/${root_name}.XXXXXX)
 }
 
 function dohelp() {
-  local exit_value=0
-  local error_message=
+    local exit_value=0
+    local error_message=
 
-  case "$#" in
-    '1')
-      exit_value=$1
-      ;;
-    '2')
-      exit_value=$1
-      shift
-      error_message="$*"
-      ;;
-  esac
-  [ -n "$error_message" ] && \
-    echo "ERROR : @" 1>&2
+    case "$#" in
+        '1')
+            exit_value=$1
+            ;;
+        '2')
+            exit_value=$1
+            shift
+            error_message="$*"
+            ;;
+    esac
+    [ -n "$error_message" ] && echo "ERROR : @" 1>&2
 
-  /usr/bin/printf "${help_text}"
+    ${PRINTF} "${help_text}"
 
-  exit ${exit_value}
+    exit ${exit_value}
 }
